b691295a29c78825b58f4e01d4f2fa81e82a1184
Pritam <spritam675@gmail.com>
2022-12-12 23:44:09 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,13 +3,13 @@ package com.driver;
 public class Main {
 public static class Product{
     public static int product(int x,int y){
-        return x+y;
+        return x*y;
     }
     public static int product(int x,int y,int z){
-        return x+y+z;
+        return x*y*z;
     }
     public static double product(double x,double y){
-        return x+y;
+        return x*y;
     }
 
     public static void main(String[] args) {
